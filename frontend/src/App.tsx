e33482dc289e05a71111cf456821c0db6daec2d2
erodrigufer <erodrigufer@gmail.com>
2023-05-07 16:52:28 +0200

    App does not work, CORS requests problem server-side

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react";
 import OutputSentence from "./components/OutputSentence";
 import WordInput from "./components/WordInput";
+import axios from "axios";
 
 function App() {
+  const [html, setHtml] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get("https://dilc.org/casa");
+      setHtml(result.data);
+    };
+    fetchData();
+  }, []);
+
+  console.log(html);
+
   const inputPlaceholder = "Enter your Catalan query here...";
   const colorScheme = "purple";
   return (
@@ -12,6 +26,7 @@ function App() {
         isLoading={false}
       />
       <OutputSentence inputString="Test string, what up! More text ipsum lorem testum rerum, morem lirum." />
+      <pre>{html}</pre>
     </>
   );
 }
