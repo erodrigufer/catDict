0ff5adb814556e59c55469a6e08efe40360c747e
erodrigufer <erodrigufer@gmail.com>
2023-05-08 15:38:17 +0200

    Show the query data in a Chakra table

--- frontend/src/components/ResultsOutput.tsx
+++ frontend/src/components/ResultsOutput.tsx
@@ -1,3 +1,11 @@
+import {
+  TableContainer,
+  Table,
+  TableCaption,
+  Tbody,
+  Tr,
+  Td,
+} from "@chakra-ui/table";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
@@ -10,12 +18,26 @@ const ResultsOutput = () => {
     queryKey: ["word"],
     queryFn: () =>
       axios
-        .get<definitions>("http://localhost:3000/definition/gos")
+        .get<definitions>("http://localhost:3000/definition/edifici")
         .then((res) => res.data),
   });
 
   return (
     <>
+      <TableContainer>
+        <Table variant="simple">
+          <TableCaption>Definicions per a...</TableCaption>
+          <Tbody>
+            {query.data?.definitions.map((definition, index) => (
+              <Tr>
+                <Td>{index + 1}</Td>
+                <Td>{definition}</Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
+
       {query.data?.definitions.map((definition, index) => (
         <p key={index}>{definition}</p>
       ))}
