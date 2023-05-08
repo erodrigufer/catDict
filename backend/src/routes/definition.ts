bbc23188778540199f03b025dc2a61960b527a15
erodrigufer <erodrigufer@gmail.com>
2023-05-08 04:23:06 +0200

    Return definitions as json to client browser

--- backend/src/routes/definition.ts
+++ backend/src/routes/definition.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 import cheerio from 'cheerio';
 
-
+export interface definitions {
+    definitions: string[]
+}
 
 export async function getDefinition(word:string) {
  // URL of the web page to scrape.
@@ -10,7 +12,7 @@ const url = 'https://dilc.org/' + word
 try {
     const response = await axios.get<string>(url)
     //  return response.data
-    scrapeDefinitions(response.data)
+    return scrapeDefinitions(response.data)
 }catch (error) {
     console.error(error)
     // throw new Error('An error took place.')
@@ -18,19 +20,25 @@ try {
 
 }
 
-function scrapeDefinitions(doc:string) {
+function scrapeDefinitions(doc:string):definitions {
+    let definitions:definitions = {
+        definitions: []
+    }
     const $ = cheerio.load(doc);
 
     // Find <ul> with class "definicio", and its <li> children.
     const lis = $('ul.definicio').find('li')
 
-    // if (lis.length === 0){
-    //     console.log(`No entries found for word '${inputUser}'.`)
-    // }
+    // No definitions found.
+    if (lis.length === 0){
+        return definitions
+    }
 
     // For each <li> element print an index and the text of the element.
     lis.each((index, element) => {
-    console.log(`${index+1}: ${$(element).text()}`);
+        definitions.definitions.push($(element).text())
+        // console.log(`${index+1}: ${$(element).text()}`);
     })
+    return definitions
 
 }
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import { getDefinition } from './routes/definition';
+import { getDefinition, definitions } from './routes/definition';
 
 const app = express();
 // TODO: CORS for local development. Remove in production.
@@ -10,20 +10,8 @@ const port = 3000;
 app.get('/definition/:word', async (req, res) => {
   const { word } = req.params;
 
-  // const url = `https://www.dictionary.com/browse/${word}`;
-
-  // try {
-  //   const response = await axios.get(url);
-  //   res.send(response.data);
-  // } catch (error) {
-  //   // res.status(500).send(error.message);
-  //   res.status(500).send('An error took place.');
-  // }
-
-// res.send(word)
-
 getDefinition(word)
-.then((resp: any) => {
+.then((resp: definitions | undefined ) => {
   res.send(resp)
 })
 .catch((err: any) => {
