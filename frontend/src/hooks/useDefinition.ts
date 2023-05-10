19719537df14bf5482d37d8e36c9ebe1d5422729
erodrigufer <erodrigufer@gmail.com>
2023-05-10 17:25:18 +0200

    Sanitize the query of a definition
    Remove points and commas from query.
    All characters are transformed to lowercase.
    All characters before an apostrophe are removed.

--- frontend/src/hooks/useDefinition.ts
+++ frontend/src/hooks/useDefinition.ts
@@ -5,8 +5,18 @@ export interface definitions {
   definitions: string[];
 }
 
+function sanitizeQuery(query: string): string {
+  // Remove all characters before an apostrophe (') (’).
+  const index = query.indexOf(`'`) !== -1 ? query.indexOf(`'`) : query.indexOf(`’`);
+  // If the apostrophe character is not found then the indexOf() method returns -1.
+  query = index  !== -1 ? query.substring(index + 1) : query;
+
+  // Remove points and commas and transform whole string to lowercase.
+  return query.toLowerCase().replace(".", "").replace(",","")
+}
+
 const useDefinition = (lookupWord: string) => {
-    const url = 'http://localhost:3000/definition/' + lookupWord
+    const url = 'http://localhost:3000/definition/' + sanitizeQuery(lookupWord)
     // Define function that fetches definitions.
     const fetchDefinitions = () =>
       axios
