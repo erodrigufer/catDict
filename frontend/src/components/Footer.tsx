acc0df970e5a9df1df9b231cd91e46397a14f748
erodrigufer <erodrigufer@gmail.com>
2023-05-12 01:39:41 +0200

    Fix color issue with footer in dark-mode

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,14 +1,12 @@
-import { Link, Text } from "@chakra-ui/react";
+import { Link, useColorModeValue } from "@chakra-ui/react";
 
 const Footer = () => {
+  // Link color definition for light-mode and dark-mode.
+  const linkColor = useColorModeValue("blackAlpha.600", "whiteAlpha.600");
   return (
     <>
-      <Link
-        href="https://github.com/erodrigufer"
-        isExternal
-        color={"blackAlpha.600"}
-      >
-        <Text> Eduardo Rodriguez &copy; 2023</Text>
+      <Link href="https://github.com/erodrigufer" isExternal color={linkColor}>
+        Eduardo Rodriguez &copy; 2023
       </Link>
     </>
   );
