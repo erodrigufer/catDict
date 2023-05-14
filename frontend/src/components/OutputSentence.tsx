7b90f776a005f70492172bff5749aff5ba74280f
erodrigufer <erodrigufer@gmail.com>
2023-05-14 20:02:30 +0200

    [Fix] Unreadable bg color of highlighted text in dark mode

--- frontend/src/components/OutputSentence.tsx
+++ frontend/src/components/OutputSentence.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Tooltip } from "@chakra-ui/react";
+import { Box, Flex, Tooltip, useColorMode } from "@chakra-ui/react";
 
 interface Props {
   inputString: string;
@@ -6,6 +6,8 @@ interface Props {
 }
 
 const OutputSentence = (props: Props) => {
+  const { colorMode } = useColorMode();
+
   // Split the string into single words stored in unique array cells.
   const singleWords = props.inputString.split(" ");
 
@@ -22,7 +24,10 @@ const OutputSentence = (props: Props) => {
             <Box
               key={index}
               mr={1}
-              _hover={{ bg: "yellow.200" }}
+              // background color on hover depends on color mode (light or dark).
+              _hover={{
+                bg: colorMode === "light" ? "yellow.200" : "green.500",
+              }}
               onClick={handleClick}
             >
               {word}
