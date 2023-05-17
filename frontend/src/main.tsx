c67bed961a9322da0581d44e8c26c6dcb2f2f7bf
erodrigufer <erodrigufer@gmail.com>
2023-05-17 16:16:49 +0200

    Change default setting of React Query provider
    - Do not refetch on windows focus.
    - Stale Time is infinite.
    - Increase cacheTime to 45min.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,9 +5,21 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App.tsx";
 
-const queryClient = new QueryClient();
-// TODO: add config parameters:
-// - Do not re-fetch on re-entering window, etc.
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // Do not re-fetch when window's focus is changed.
+      refetchOnWindowFocus: false,
+      // Never re-fetch any data that has already been cached.
+      staleTime: Infinity,
+      // Do not refetch data if network connection is re-established.
+      refetchOnReconnect: false,
+      // After the cacheTime, the inactive data is garbage collected,
+      // i.e. it would need to be refetched once more.
+      cacheTime: 45 * 60 * 1000, // in ms
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
