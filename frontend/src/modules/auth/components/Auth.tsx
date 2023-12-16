eb2ee1bc9438386ba88faa7f6b4d62eedb1c44ad
erodrigufer <erodrigufer@gmail.com>
2023-12-16 18:16:54 +0100

    Handle submit button

--- frontend/src/modules/auth/components/Auth.tsx
+++ frontend/src/modules/auth/components/Auth.tsx
@@ -1,14 +1,33 @@
-import { Box, Flex, Input } from "@chakra-ui/react";
+import { Box, Button, Flex, Input } from "@chakra-ui/react";
+import { FormEvent } from "react";
+
+const handleSubmit = (event: FormEvent) => {
+  event.preventDefault();
+
+  console.log("Submit username and password");
+};
 
 const Auth: React.FC = () => {
   return (
     <>
       <Box marginTop={4} marginBottom={4}>
-        <Flex direction="column" gap={4}>
-          <Input placeholder="Nom d'usuari" boxShadow="base" variant="filled" />
-          {/* TODO: Check that the word for password is correct in Catalan */}
-          <Input placeholder="Contrasenya" boxShadow="base" variant="filled" />
-        </Flex>
+        <form onSubmit={handleSubmit}>
+          <Flex direction="column" gap={4}>
+            <Input
+              placeholder="Nom d'usuari"
+              boxShadow="base"
+              variant="filled"
+            />
+            {/* TODO: Check that the word for password is correct in Catalan */}
+            <Input
+              placeholder="Contrasenya"
+              boxShadow="base"
+              variant="filled"
+            />
+            {/* TODO: Check Button of WOrdInput.tsx to complete this button */}
+            <Button type="submit">Enter</Button>
+          </Flex>
+        </form>
       </Box>
     </>
   );
