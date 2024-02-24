a950f49593155118ba6c5ce6f4b88294c6c417f1
erodrigufer <erodrigufer@gmail.com>
2024-02-24 13:41:22 +0100

    Add health check to aws_route53 resource

--- terraform/webapp/domain.tf
+++ terraform/webapp/domain.tf
@@ -18,6 +18,17 @@ resource "aws_route53_record" "backend" {
   ttl     = "300"
 
   records = [aws_instance.ec2_instance.public_ip]
+}
 
+resource "aws_route53_health_check" "example" {
+  fqdn              = aws_route53_record.backend[0].fqdn
+  port              = 80
+  type              = "HTTP"
+  resource_path     = "/"
+  failure_threshold = "5"
+  request_interval  = "30"
 
+  tags = {
+    Name = "catDict-health-check"
+  }
 }
