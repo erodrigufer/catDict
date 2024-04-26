348c63f703e605f08b445acd5628b595bf8fe50d
erodrigufer <erodrigufer@gmail.com>
2024-04-26 19:09:19 +0200

    Create subnet for EC2 instance

--- terraform/webapp/instance.tf
+++ terraform/webapp/instance.tf
@@ -1,3 +1,7 @@
+data "aws_availability_zones" "azs" {
+  state = "available"
+}
+
 resource "aws_launch_template" "launch_template" {
   name_prefix   = var.name
   image_id      = var.ami_id
@@ -12,10 +16,15 @@ resource "aws_launch_template" "launch_template" {
 
 resource "aws_instance" "ec2_instance" {
   vpc_security_group_ids = [aws_security_group.sg_ec2.id]
+  subnet_id = aws_default_subnet.default_az1.id
 
   launch_template {
     id = aws_launch_template.launch_template.id
   }
+}
+
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = data.aws_availability_zones.azs.names[0]
 }
 
 resource "aws_security_group" "sg_ec2" {
